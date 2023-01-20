fb3dd10340bb63a6cf09c0e97232ac19f4a3cfc7
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-01-20 19:51:05 +0100

    modified:   Arduino/servo plus senzor/servo plus senzor.ino

--- Arduino/servo plus senzor/servo plus senzor.ino
+++ Arduino/servo plus senzor/servo plus senzor.ino
@@ -1,20 +1,29 @@
 #include <DHT.h>
 #include <Servo.h>
 
-#define joze 3
-#define vidko 4
+#define servo_pin 3
+#define dht_pin 4
 
 Servo mojservo;
 
-DHT dht = DHT(vidko, DHT11);
+DHT dht = DHT(dht_pin, DHT11);
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+
+  dht.begin();
+  mojservo.attach(servo_pin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(dht.readHumidity());
+  Serial.print("  ");
+  Serial.println(dht.readTemperature());
+  delay(100);
+
 
 }
