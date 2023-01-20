022aae4ab175e5c1e46890e91c8691e8d150aabe
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-01-20 19:38:00 +0100

    modified:   Arduino/servo plus senzor/servo plus senzor.ino

--- Arduino/servo plus senzor/servo plus senzor.ino
+++ Arduino/servo plus senzor/servo plus senzor.ino
@@ -1,3 +1,14 @@
+#include <DHT.h>
+#include <Servo.h>
+
+#define joze 3
+#define vidko 4
+
+Servo mojservo;
+
+DHT dht = DHT(vidko, DHT11);
+
+
 void setup() {
   // put your setup code here, to run once:
 
