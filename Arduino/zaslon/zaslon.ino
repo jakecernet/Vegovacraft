2470627f6d6d77d043f2adfd07bbc14a7757875c
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-01-27 19:59:27 +0100

    new file:   Arduino/zaslon/zaslon.ino

---
+++ Arduino/zaslon/zaslon.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.cursor();
+  lcd.blink();
+
+  delay(1000);
+  // Print a message to the LCD.
+  lcd.print("To je pa eno daljse besedilo. Pravzaprav je zares dolgo.");
+  lcd.home();
+}
+
+void loop() {
+  for (int i = 0; i < 24; i++) {
+    lcd.scrollDisplayLeft();
+    delay(400);
+  }
+
+  delay(2000);
+
+  for (int i = 0; i < 24; i++) {
+    lcd.scrollDisplayRight();
+    delay(400);
+  }
+
+  delay(2000);
+}
