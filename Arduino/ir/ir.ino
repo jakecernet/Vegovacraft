0a7cdc13a18f695d3fb05ffdbfaab13ab0bcafcc
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-03-10 20:41:22 +0100

    modified:   Arduino/ir/ir.ino 	new file:   Arduino/libraries/LiquidCrystal/README.adoc 	new file:   Arduino/libraries/LiquidCrystal/examples/Autoscroll/Autoscroll.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/Blink/Blink.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/Cursor/Cursor.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/CustomCharacter/CustomCharacter.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/Display/Display.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/HelloWorld/HelloWorld.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/Scroll/Scroll.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/SerialDisplay/SerialDisplay.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/TextDirection/TextDirection.ino 	new file:   Arduino/libraries/LiquidCrystal/examples/setCursor/setCursor.ino 	new file:   Arduino/libraries/LiquidCrystal/keywords.txt 	new file:   Arduino/libraries/LiquidCrystal/library.properties 	new file:   Arduino/libraries/LiquidCrystal/src/LiquidCrystal.cpp 	new file:   Arduino/libraries/LiquidCrystal/src/LiquidCrystal.h 	renamed:    "Arduino/zvo\304\215nik/zvo\304\215nik.ino" -> Arduino/zvocnik/zvocnik.ino

--- Arduino/ir/ir.ino
+++ Arduino/ir/ir.ino
@@ -1,4 +1,4 @@
- #include <IRremote.hpp>
+#include <IRremote.hpp>
 #include <FastLED.h>
 #include <LiquidCrystal.h>
 #define DECODE_NEC
@@ -30,26 +30,30 @@ CRGB leds[NUM_LEDS];
 bool onoff = false;
 int pozicija = 0;
 
-void nastaviLED(int pozicija){
-  for (int i = 0; i < NUM_LEDS; i++){
+/*void nastaviLED(int pozicija)
+{
+  for (int i = 0; i < NUM_LEDS; i++)
+  {
     leds[i] = CRGB::Black;
   }
-  if (pozicija >= NUM_LEDS || pozicija < 0) return;
+  if (pozicija >= NUM_LEDS || pozicija < 0)
+    return;
   leds[pozicija] = CRGB::White;
 
   FastLED.show();
 }
 
 const int rs = 12, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);*/
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   IrReceiver.begin(A0, ENABLE_LED_FEEDBACK);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   lcd.begin(16, 2);
   lcd.print("Jaka Cernetic");
-  
+
   pinMode(SW, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   onoff = digitalRead(SW);
@@ -60,171 +64,176 @@ bool joy_sw_now = false;
 int svetlost = 0;
 unsigned long cooldown = millis();
 
-void loop() {
+void loop()
+{
 
   int joy_x = analogRead(JOY_X);
   int joy_y = analogRead(JOY_Y);
   joy_sw_now = !digitalRead(SW);
 
-  if (joy_sw_old == false && joy_sw_now == true) {
+  if (joy_sw_old == false && joy_sw_now == true)
+  {
     onoff != onoff;
     digitalWrite(13, onoff);
   }
-  joy_sw_old = joy_sw_now;  
+  joy_sw_old = joy_sw_now;
 
-  svetlost = joy_y/4;
+  svetlost = joy_y / 4;
   FastLED.setBrightness(svetlost);
 
-  if (joy_x < 250 && pozicija > 0) {
+  if (joy_x < 250 && pozicija > 0)
+  {
     pozicija--;
   }
-  else if (joy_x > 750 && pozicija < NUM_LEDS){
+  else if (joy_x > 750 && pozicija < NUM_LEDS)
+  {
     pozicija++;
   }
 
-  nastaviLED(pozicija);
-
-  if (IrReceiver.decode()) {
+  if (IrReceiver.decode())
+  {
     unsigned long koda = IrReceiver.decodedIRData.decodedRawData;
-    switch (koda) {
-      case TIPKA_1:
-        IZPISI_TIPKO(TIPKA_1)
-        leds[0] = CRGB::Red;
-        leds[1] = CRGB::Red;
-        leds[2] = CRGB::Red;
-        leds[3] = CRGB::Red;
-        leds[4] = CRGB::Red;
-        leds[5] = CRGB::Red;
-        leds[6] = CRGB::Red;
-        leds[7] = CRGB::Red;
-        leds[8] = CRGB::Red;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Red");
-        break;
-      case TIPKA_2:
-        IZPISI_TIPKO(TIPKA_2)
-        leds[0] = CRGB::Green;
-        leds[1] = CRGB::Green;
-        leds[2] = CRGB::Green;
-        leds[3] = CRGB::Green;
-        leds[4] = CRGB::Green;
-        leds[5] = CRGB::Green;
-        leds[6] = CRGB::Green;
-        leds[7] = CRGB::Green;
-        leds[8] = CRGB::Green;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Green");
-        break;
-      case TIPKA_3:
-        IZPISI_TIPKO(TIPKA_3)
-        leds[0] = CRGB::Blue;
-        leds[1] = CRGB::Blue;
-        leds[2] = CRGB::Blue;
-        leds[3] = CRGB::Blue;
-        leds[4] = CRGB::Blue;
-        leds[5] = CRGB::Blue;
-        leds[6] = CRGB::Blue;
-        leds[7] = CRGB::Blue;
-        leds[8] = CRGB::Blue;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Blue");
-        break;
-      case TIPKA_4:
-        IZPISI_TIPKO(TIPKA_4)
-        leds[0] = CRGB::White;
-        leds[1] = CRGB::White;
-        leds[2] = CRGB::White;
-        leds[3] = CRGB::White;
-        leds[4] = CRGB::White;
-        leds[5] = CRGB::White;
-        leds[6] = CRGB::White;
-        leds[7] = CRGB::White;
-        leds[8] = CRGB::White;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("White");
-        break;
-      case TIPKA_5:
-        IZPISI_TIPKO(TIPKA_5)
-        leds[0] = CRGB::Black;
-        leds[1] = CRGB::Black;
-        leds[2] = CRGB::Black;
-        leds[3] = CRGB::Black;
-        leds[4] = CRGB::Black;
-        leds[5] = CRGB::Black;
-        leds[6] = CRGB::Black;
-        leds[7] = CRGB::Black;
-        leds[8] = CRGB::Black;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Off");
-        break;
-      case TIPKA_6:
-        IZPISI_TIPKO(TIPKA_6)
-        leds[0] = CRGB::Yellow;
-        leds[1] = CRGB::Yellow;
-        leds[2] = CRGB::Yellow;
-        leds[3] = CRGB::Yellow;
-        leds[4] = CRGB::Yellow;
-        leds[5] = CRGB::Yellow;
-        leds[6] = CRGB::Yellow;
-        leds[7] = CRGB::Yellow;
-        leds[8] = CRGB::Yellow;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Yellow");
-        break;
-      case TIPKA_7:
-        IZPISI_TIPKO(TIPKA_7)
-        leds[0] = CRGB::Purple;
-        leds[1] = CRGB::Purple;
-        leds[2] = CRGB::Purple;
-        leds[3] = CRGB::Purple;
-        leds[4] = CRGB::Purple;
-        leds[5] = CRGB::Purple;
-        leds[6] = CRGB::Purple;
-        leds[7] = CRGB::Purple;
-        leds[8] = CRGB::Purple;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Purple");
-        break;
-      case TIPKA_8:
-        IZPISI_TIPKO(TIPKA_8)
-        leds[0] = CRGB::Red;
-        leds[1] = CRGB::Orange;
-        leds[2] = CRGB::Yellow;
-        leds[3] = CRGB::White;
-        leds[4] = CRGB::Cyan;
-        leds[5] = CRGB::Blue;
-        leds[6] = CRGB::Green;
-        leds[7] = CRGB::Purple;
-        leds[8] = CRGB::DarkRed;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Rainbow");
-        break;
-      case TIPKA_9:
-        IZPISI_TIPKO(TIPKA_9)
-        leds[0] = CRGB::Cyan;
-        leds[1] = CRGB::Cyan;
-        leds[2] = CRGB::Cyan;
-        leds[3] = CRGB::Cyan;
-        leds[4] = CRGB::Cyan;
-        leds[5] = CRGB::Cyan;
-        leds[6] = CRGB::Cyan;
-        leds[7] = CRGB::Cyan;
-        leds[8] = CRGB::Cyan;
-        FastLED.show();
-        lcd.clear();
-        lcd.print("Cyan");
-        break;
-      default:
-        if (koda != 0) Serial.println(koda, HEX);
-        break;
+    switch (koda)
+    {
+    case TIPKA_1:
+      IZPISI_TIPKO(TIPKA_1)
+      leds[0] = CRGB::Red;
+      leds[1] = CRGB::Red;
+      leds[2] = CRGB::Red;
+      leds[3] = CRGB::Red;
+      leds[4] = CRGB::Red;
+      leds[5] = CRGB::Red;
+      leds[6] = CRGB::Red;
+      leds[7] = CRGB::Red;
+      leds[8] = CRGB::Red;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Red");
+      break;
+    case TIPKA_2:
+      IZPISI_TIPKO(TIPKA_2)
+      leds[0] = CRGB::Green;
+      leds[1] = CRGB::Green;
+      leds[2] = CRGB::Green;
+      leds[3] = CRGB::Green;
+      leds[4] = CRGB::Green;
+      leds[5] = CRGB::Green;
+      leds[6] = CRGB::Green;
+      leds[7] = CRGB::Green;
+      leds[8] = CRGB::Green;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Green");
+      break;
+    case TIPKA_3:
+      IZPISI_TIPKO(TIPKA_3)
+      leds[0] = CRGB::Blue;
+      leds[1] = CRGB::Blue;
+      leds[2] = CRGB::Blue;
+      leds[3] = CRGB::Blue;
+      leds[4] = CRGB::Blue;
+      leds[5] = CRGB::Blue;
+      leds[6] = CRGB::Blue;
+      leds[7] = CRGB::Blue;
+      leds[8] = CRGB::Blue;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Blue");
+      break;
+    case TIPKA_4:
+      IZPISI_TIPKO(TIPKA_4)
+      leds[0] = CRGB::White;
+      leds[1] = CRGB::White;
+      leds[2] = CRGB::White;
+      leds[3] = CRGB::White;
+      leds[4] = CRGB::White;
+      leds[5] = CRGB::White;
+      leds[6] = CRGB::White;
+      leds[7] = CRGB::White;
+      leds[8] = CRGB::White;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("White");
+      break;
+    case TIPKA_5:
+      IZPISI_TIPKO(TIPKA_5)
+      leds[0] = CRGB::Black;
+      leds[1] = CRGB::Black;
+      leds[2] = CRGB::Black;
+      leds[3] = CRGB::Black;
+      leds[4] = CRGB::Black;
+      leds[5] = CRGB::Black;
+      leds[6] = CRGB::Black;
+      leds[7] = CRGB::Black;
+      leds[8] = CRGB::Black;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Off");
+      break;
+    case TIPKA_6:
+      IZPISI_TIPKO(TIPKA_6)
+      leds[0] = CRGB::Yellow;
+      leds[1] = CRGB::Yellow;
+      leds[2] = CRGB::Yellow;
+      leds[3] = CRGB::Yellow;
+      leds[4] = CRGB::Yellow;
+      leds[5] = CRGB::Yellow;
+      leds[6] = CRGB::Yellow;
+      leds[7] = CRGB::Yellow;
+      leds[8] = CRGB::Yellow;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Yellow");
+      break;
+    case TIPKA_7:
+      IZPISI_TIPKO(TIPKA_7)
+      leds[0] = CRGB::Purple;
+      leds[1] = CRGB::Purple;
+      leds[2] = CRGB::Purple;
+      leds[3] = CRGB::Purple;
+      leds[4] = CRGB::Purple;
+      leds[5] = CRGB::Purple;
+      leds[6] = CRGB::Purple;
+      leds[7] = CRGB::Purple;
+      leds[8] = CRGB::Purple;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Purple");
+      break;
+    case TIPKA_8:
+      IZPISI_TIPKO(TIPKA_8)
+      leds[0] = CRGB::Red;
+      leds[1] = CRGB::Orange;
+      leds[2] = CRGB::Yellow;
+      leds[3] = CRGB::White;
+      leds[4] = CRGB::Cyan;
+      leds[5] = CRGB::Blue;
+      leds[6] = CRGB::Green;
+      leds[7] = CRGB::Purple;
+      leds[8] = CRGB::DarkRed;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Rainbow");
+      break;
+    case TIPKA_9:
+      IZPISI_TIPKO(TIPKA_9)
+      leds[0] = CRGB::Cyan;
+      leds[1] = CRGB::Cyan;
+      leds[2] = CRGB::Cyan;
+      leds[3] = CRGB::Cyan;
+      leds[4] = CRGB::Cyan;
+      leds[5] = CRGB::Cyan;
+      leds[6] = CRGB::Cyan;
+      leds[7] = CRGB::Cyan;
+      leds[8] = CRGB::Cyan;
+      FastLED.show();
+      lcd.clear();
+      lcd.print("Cyan");
+      break;
+    default:
+      if (koda != 0)
+        Serial.println(koda, HEX);
+      break;
     }
     IrReceiver.resume();
   }
