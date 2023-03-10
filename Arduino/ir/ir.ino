46e28eac8cd773609a4877ddaa01abdeb51bebc8
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-03-10 19:57:32 +0100

    modified:   Arduino/ir/ir.ino

--- Arduino/ir/ir.ino
+++ Arduino/ir/ir.ino
@@ -2,7 +2,6 @@
 #include <FastLED.h>
 #include <LiquidCrystal.h>
 #define DECODE_NEC
-#define MOJ_DALJINC 2
 #define IZPISI_TIPKO(x) Serial.println(#x);
 #define NUM_LEDS 9
 #define DATA_PIN 11
@@ -11,59 +10,42 @@
 #define JOY_Y A4
 #define SW A3
 
-#if MOJ_DALJINC == 1
-  #define TIPKA_ZVEZDICA 0xE916FF00 
-  #define TIPKA_LOJTRA 0xF20DFF00
-  #define TIPKA_0 0xE619FF00
-  #define TIPKA_1 0xBA45FF00
-  #define TIPKA_2 0xB946FF00
-  #define TIPKA_3 0xB847FF00
-  #define TIPKA_4 0xBB44FF00
-  #define TIPKA_5 0xBF40FF00
-  #define TIPKA_6 0xBC43FF00
-  #define TIPKA_7 0xF807FF00
-  #define TIPKA_8 0xEA15FF00
-  #define TIPKA_9 0xF609FF00
-#elif MOJ_DALJINC == 2
-  #define TIPKA_ZVEZDICA 0
-  #define TIPKA_LOJTRA 0
-  #define TIPKA_0 0xE916FF00
-  #define TIPKA_100PLUS 0xE619FF00
-  #define TIPKA_200PLUS 0xF20DFF00
-  #define TIPKA_1 0xF30CFF00
-  #define TIPKA_2 0xE718FF00
-  #define TIPKA_3 0xA15EFF00
-  #define TIPKA_4 0xF708FF00
-  #define TIPKA_5 0xE31CFF00
-  #define TIPKA_6 0xA55AFF00
-  #define TIPKA_7 0xBD42FF00
-  #define TIPKA_8 0xAD52FF00
-  #define TIPKA_9 0xB54AFF00
-#endif
+#define TIPKA_ZVEZDICA 0
+#define TIPKA_LOJTRA 0
+#define TIPKA_0 0xE916FF00
+#define TIPKA_100PLUS 0xE619FF00
+#define TIPKA_200PLUS 0xF20DFF00
+#define TIPKA_1 0xF30CFF00
+#define TIPKA_2 0xE718FF00
+#define TIPKA_3 0xA15EFF00
+#define TIPKA_4 0xF708FF00
+#define TIPKA_5 0xE31CFF00
+#define TIPKA_6 0xA55AFF00
+#define TIPKA_7 0xBD42FF00
+#define TIPKA_8 0xAD52FF00
+#define TIPKA_9 0xB54AFF00
 
 CRGB leds[NUM_LEDS];
 
+bool onoff = false;
+
 const int rs = 12, en = 10, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-bool onoff = false;
 
 void setup() {
   Serial.begin(115200);
   IrReceiver.begin(A0, ENABLE_LED_FEEDBACK);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   lcd.begin(16, 2);
-  lcd.print("Hello, world!");
+  lcd.print("Use IR remote and joystick.");
   
   pinMode(SW, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
   onoff = digitalRead(SW);
-
 }
 
 bool joy_sw_old = false;
 bool joy_sw_now = false;
-
 int svetlost = 0;
 
 void loop() {
@@ -74,21 +56,15 @@ void loop() {
 
   if (joy_sw_old == false && joy_sw_now == true) {
     onoff != onoff;
-     digitalWrite(13, onoff);
+    digitalWrite(13, onoff);
   }
-
-  joy_sw_old = joy_sw_now;
+  joy_sw_old = joy_sw_now;  
 
   svetlost = analogRead(A5)/4;
-
   FastLED.setBrightness(svetlost);
 
   if (IrReceiver.decode()) {
     unsigned long koda = IrReceiver.decodedIRData.decodedRawData;
-
-    Serial.println("Dobil pritisk.");
-
-    // Glede na prejeto kodo skočimo na določen del programa.
     switch (koda) {
       case TIPKA_1:
         IZPISI_TIPKO(TIPKA_1)
@@ -231,6 +207,5 @@ void loop() {
     }
     IrReceiver.resume();
   }
-
   FastLED.show();
 }
