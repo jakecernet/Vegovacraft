33315eb28d8217bc599d4a8d3af62adb8dd550a7
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-09-13 21:25:11 +0200

    new file:   .vscode/arduino.json 	modified:   Arduino/nfc/nfc.ino

--- Arduino/nfc/nfc.ino
+++ Arduino/nfc/nfc.ino
@@ -11,28 +11,36 @@
 
 MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
-byte modri_tag[4] = {0xBD, 0x99, 0xFE, 0x30}; //defines the UID of the card
-byte dijaska[4] = {0x89, 0x8D, 0x79, 0x5E};   //defines the UID of the card
-byte woop[4] = {0x0E, 0x2E, 0xA3, 0x45};      //defines the UID of the card
+byte modri_tag[4] = {0xBD, 0x99, 0xFE, 0x30}; // defines the UID of the card
+byte dijaska[4] = {0x89, 0x8D, 0x79, 0x5E};   // defines the UID of the card
+byte woop[4] = {0x0E, 0x2E, 0xA3, 0x45};      // defines the UID of the card
 
-const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;    // LCD pinout
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                   // LCD object
+const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8; // LCD pinout
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);                // LCD object
 
-void success(){ //function for successfull card reading
-  tone(SOUND, 400);     //plays a tone
-  delay(200);           //waits 200ms
-  tone(SOUND, 800);     //plays a tone
-  delay(200);           
+void success()
+{                   // function for successfull card reading
+  tone(SOUND, 400); // plays a tone
+  delay(200);       // waits 200ms
+  tone(SOUND, 800); // plays a tone
+  delay(200);
   tone(SOUND, 1200);
   delay(200);
-  noTone(SOUND);        
+  noTone(SOUND);
   digitalWrite(GREEN, HIGH);
   digitalWrite(RED, LOW);
 }
 
-void fail(){      //function for unsuccessfull card reading
+void fail()
+{ // function for unsuccessfull card reading
   lcd.clear();
-  lcd.print("Napacna kartica");
+  lcd.print("Neznana kartica");
+  lcd.setCursor(0, 1);
+  lcd.print("UID: ");
+  lcd.print(rfid.uid.uidByte[0], HEX);
+  lcd.print(rfid.uid.uidByte[1], HEX);
+  lcd.print(rfid.uid.uidByte[2], HEX);
+  lcd.print(rfid.uid.uidByte[3], HEX);
   digitalWrite(GREEN, HIGH);
   tone(SOUND, 2000);
   delay(200);
@@ -40,7 +48,7 @@ void fail(){      //function for unsuccessfull card reading
   delay(100);
   noTone(SOUND);
   digitalWrite(RED, HIGH);
-  delay(1000);
+  delay(4000);
   digitalWrite(GREEN, LOW);
 }
 
@@ -98,6 +106,17 @@ void loop()
     lcd.clear();
     lcd.print("Modri tag");
     Serial.println("Modri tag");
+    lcd.setCursor(0, 1);
+    lcd.print("UID: ");
+    lcd.print(rfid.uid.uidByte[0], HEX);
+    lcd.print(rfid.uid.uidByte[1], HEX);
+    lcd.print(rfid.uid.uidByte[2], HEX);
+    lcd.print(rfid.uid.uidByte[3], HEX);
+    delay(4000);
+    lcd.clear();
+    lcd.print("Prisloni kartico");
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
   }
   else
   {
