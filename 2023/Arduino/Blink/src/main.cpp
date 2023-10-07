b3122e7d59ee4b2766e6c8b197a98f99c4b261d9
jakecernet <jaka.cernetic@students.vegova.si>
2023-10-07 20:26:14 +0200

    modified:   2023/Arduino/Blink/platformio.ini 	modified:   2023/Arduino/Blink/src/main.cpp

--- 2023/Arduino/Blink/src/main.cpp
+++ 2023/Arduino/Blink/src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 
-void setup(){
+#define color_red "\x1B[31m"
+#define color_yellow "\x1B[33m"
+#define color_reset "\x1B[0m"
+
+void setup()
+{
   pinMode(LED_BUILTIN, OUTPUT);
   serial.begin(115200);
 };
 
-void loop(){
+void loop()
+{
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print(color_red "led on"color_reset);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.print(color_yellow "led off" color_reset);
   delay(1000);
 };
