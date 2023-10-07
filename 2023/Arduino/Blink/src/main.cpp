1aaa409f3f45b9c3f8b51b6dcb78e4f49b45a5b0
jakecernet <jaka.cernetic@students.vegova.si>
2023-10-07 20:59:23 +0200

    ANSI dodatek

--- 2023/Arduino/Blink/src/main.cpp
+++ 2023/Arduino/Blink/src/main.cpp
@@ -1,13 +1,28 @@
 #include <Arduino.h>
 
+#define ANSI 1
+
+#if ANSI
+
 #define color_red "\x1B[31m"
 #define color_white "\x1B[37m"
 #define color_yellow "\x1B[33m"
 #define color_reset "\x1B[0m"
-
 #define GOTOXY(x, y) "\e[" #y ";" #x "H"
 #define clear_screen "\x1b[0K"
 #define clr_scr "\x1b[2J"
+
+#else
+
+#define color_red ""
+#define color_white ""
+#define color_yellow ""
+#define color_reset ""
+#define GOTOXY(x, y) ""
+#define clear_screen ""
+#define clr_scr ""
+
+#endif
 
 void setup()
 {
