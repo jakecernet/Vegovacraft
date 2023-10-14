ab74b400708623f31789edbf7ec2fe6a88f5883f
jakecernet <jaka.cernetic@students.vegova.si>
2023-10-14 20:28:28 +0200

    modified:   2023/Arduino/Serial read test/src/main.cpp

--- 2023/Arduino/Serial read test/src/main.cpp
+++ 2023/Arduino/Serial read test/src/main.cpp
@@ -6,14 +6,34 @@ void setup()
   Serial.begin(115200);
 }
 
+char buffer[20]; // rezerviramo 20 znakov za znake, ki jih bomo prebrali s serijskega vmesnika, začenjamo šteti od 0
+//buffer[0] je prvi znak, buffer[1] je drugi znak, itd.
+
+// brali bomo s serijskega vmesnika vse dokler uporanik ne pritisne enter ali pa prebere 20 znakov
 void loop()
 {
-  char c;
+  int i = 0;
+
+  char c; //začasna spremenljivka
 
   while (Serial.available())
   {
+    //preberemo znak iz serijskega vmesnika
     c = Serial.read();
-    Serial.print(c, HEX);
-    Serial.print(" ");
+
+    if ((c >= ' ') && (c <= 127)){
+      Serial.print(c);
+      buffer[i] = c;
+      i++;
+      if (i >= 20){
+        Serial.println("");
+        Serial.println(buffer);
+        i = 0;
+      }
+    }
   }
+
+  //na koncu izpišemo vse prejete znake
+  //Serial.println("");
+  //Serial.println(buffer);
 }
